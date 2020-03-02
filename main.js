b495df8b86238f997a22f0d25190979ef9799cb7
Vasiliy <vbbatmaev@gmail.com>
2020-03-03 01:06:56 +0300

    Верхний прямоугольник

--- main.js
+++ main.js
@@ -7,22 +7,25 @@ function get3arrays(randFunc, N, seed){
     return {avg: expArr, disp: dispArr, stdDev: stdDevArr }
 }
 
+const builtInColor = "rgba(120, 120, 255, 1)"
+const linCongColor = "rgba(255, 120, 120, 1)"
+const fiboColor = "rgba(240, 240, 120, 1)"
+
 function main(event){
     event.preventDefault()
 
-    const builtInColor = "rgba(120, 120, 255, 1)"
-    const linCongColor = "rgba(255, 120, 120, 1)"
-
-    const N = 700
+    const N = oneForm.value
     const seed = 32766
     let Xarr = simpleArray(N)
     let builtInResults = get3arrays(mathRandArray, N, 2)
     let LinCongResults = get3arrays(linearCongruent, N, seed)
+    let FibResults = get3arrays(Fibonacci, N, seed)
 
     let expObj = document.getElementById("avg")
     const XYCLsForExp = [
         {X: Xarr, Y: builtInResults.avg, color: builtInColor, lines: true},
         {X: Xarr, Y: LinCongResults.avg, color: linCongColor, lines: true},
+        {X: Xarr, Y: FibResults.avg, color: fiboColor, lines: true},
     ]
     expObj.drawGraph(XYCLsForExp, 0, 1)
 
@@ -30,6 +33,7 @@ function main(event){
     const XYCLsForDisp = [
         {X: Xarr, Y: builtInResults.disp, color: builtInColor, lines: true},
         {X: Xarr, Y: LinCongResults.disp, color: linCongColor, lines: true},
+        {X: Xarr, Y: FibResults.disp, color: fiboColor, lines: true},
     ]
     dispObj.drawGraph(XYCLsForDisp, 0, 1)
 
@@ -37,37 +41,8 @@ function main(event){
     const XYCLsForStdDev = [
         {X: Xarr, Y: builtInResults.stdDev, color: builtInColor, lines: true},
         {X: Xarr, Y: LinCongResults.stdDev, color: linCongColor, lines: true},
+        {X: Xarr, Y: FibResults.stdDev, color: fiboColor, lines: true},
     ]
     stdDevObj.drawGraph(XYCLsForStdDev, 0, 1)
 
 }
-
-// function simulationFor1R(event){
-//     event.preventDefault()
-//     let N = Number(SingleRform.N.value)
-//     let r = Number(SingleRform.r.value)
-//     let x0 = Number(SingleRform.x0.value)
-//     let diff = 0.1
-
-//     const x_n_obj = document.getElementById("x_n")
-//     let Yarr = simulate(N, r, x0)
-//     let Xarr = simpleArray(N)
-//     x_n_obj.drawGraph({X: Xarr, Y: Yarr}, true, 0, 1)
-
-//     const err_obj = document.getElementById("err")
-//     let Yarr2 = simulate(N, r, x0 + diff)
-//     let Yarr3 = Yarr.map((value, index) => Math.abs(value - Yarr2[index])/value)
-//     err_obj.drawGraph({X: Xarr, Y:Yarr3}, true, 0)
-// }
-
-// function bifurkation(event){
-//     event.preventDefault()
-//     let N = Number(multipleRs.N.value)
-//     let R_min = Number(multipleRs.R_min.value)
-//     let R_max = Number(multipleRs.R_max.value)
-//     let x0 = Number(multipleRs.x0.value)
-    
-//     const bifurk_obj = document.getElementById("bifurk")
-//     let XYPair = steadyState(N, R_min, R_max, x0)
-//     bifurk_obj.drawGraph(XYPair, false, 0, 1)
-// }
