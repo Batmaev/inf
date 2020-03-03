cdccb94372e5e526887461490e846ff80cb2bec5
Vasiliy <vbbatmaev@gmail.com>
2020-03-03 18:46:44 +0300

    fix seed в цикле

--- calculations.js
+++ calculations.js
@@ -12,13 +12,15 @@ function simulate(Nparticles, probabilityDisappear, Nsteps, randFunc, seed){
     resArr[0] = Nparticles
 
     for(let k = 1; k < Nsteps; ++k){
-        let randArr = randFunc(resArr[k - 1])
+        let randArr = randFunc(resArr[k - 1], seed)
+        seed = randArr[randArr.length - 1] //костыль для разных seed каждый раз
         resArr[k] = 0
         randArr.forEach(element => {
             if(element > probabilityDisappear){
                 resArr[k] += 1
             }
         });
+
     }
     return resArr
 }
--- events.js
+++ events.js
@@ -2,6 +2,12 @@ const oneForm = document.forms.one
 oneForm.addEventListener("submit", main.bind(this))
 oneForm.Nsteps.value = Math.floor(document.getElementById("avg").querySelector("canvas").getBoundingClientRect().width / 2)
 
+oneForm.addEventListener("keypress", event => {
+    if(event.keyCode == 13){
+        oneForm.dispatchEvent(new Event("submit"))
+    }
+})
+
 function setTime(){
     oneForm.seed.value = (new Date()).getTime() % 4294967296
     oneForm.dispatchEvent(new Event("submit"))
