0396b3179c1567e461b7261f684d07104e4bd3f7
Vasiliy <vbbatmaev@gmail.com>
2020-03-08 16:39:57 +0300

    fix обновление страницы в firefox

--- events.js
+++ events.js
@@ -4,5 +4,5 @@ oneForm.N.value = Math.floor(document.getElementById("avg").querySelector("canva
 
 function setTime(){
     oneForm.seed.value = (new Date()).getTime() % 4294967296
-    oneForm.dispatchEvent(new Event("submit"))
+    oneForm.dispatchEvent(new Event("submit", {cancelable: true}))
 }
